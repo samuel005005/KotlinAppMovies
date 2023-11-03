c16ab39ed5b92a6b6cf3c355a370b68672942485
Samuel Paez <samuelpaez@bdp.com.do>
2023-11-03 14:34:50 -0400

    organizando proyecto

---
+++ app/src/main/java/com/pokemon/moviesapp/di/AppModel.kt
@@ -0,0 +1,30 @@
+package com.pokemon.moviesapp.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object   AppModel {
+    @Singleton
+    @Provides
+    fun providePokemonRepository(
+        api: PokeApi
+    ) = PokemonRepository(api)
+
+    @Singleton
+    @Provides
+    fun providePokeApi(): PokeApi {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PokeApi::class.java);
+    }
+
+}
