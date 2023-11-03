9b3bb6cfda52c6e2d7b53ca3c51fb25b0770aab0
Samuel Paez <samuelpaez@bdp.com.do>
2023-11-03 14:23:49 -0400

    App main

--- app/src/main/java/com/pokemon/moviesapp/MainActivity.kt
+++ app/src/main/java/com/pokemon/moviesapp/MainActivity.kt
@@ -3,26 +3,43 @@ package com.pokemon.moviesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.pokemon.moviesapp.presentation.theme.MoviesAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val itemsList = (0..5).toList()
+            val itemModifier = Modifier
+                .border(1.dp, Color.Blue)
+                .width(80.dp)
+                .wrapContentSize()
+            val itemsIndexedList = listOf("A", "B", "C")
+
             MoviesAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HorizontalListView()
                 }
             }
         }
@@ -30,17 +47,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun HorizontalListView() {
+    val items = (1..50).toList()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MoviesAppTheme {
-        Greeting("Android")
+    LazyRow(
+        contentPadding = PaddingValues(horizontal = 10.dp, vertical = 20.dp),
+        modifier = Modifier.height(380.dp)
+    ) {
+        items(items) { item ->
+            Text(
+                text = "Item $item",
+                modifier = Modifier
+                    .padding(end = 10.dp)
+                    .background(color = Color.LightGray)
+                    .padding(16.dp)
+            )
+        }
     }
 }
