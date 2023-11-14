e376c0738f36f2f51dd667a323c88b3ef19ffb0f
Samuel Paez <samuelpaez@bdp.com.do>
2023-11-14 16:19:32 -0400

    replace proyect

--- app/src/main/java/com/movies/db/App.kt
+++ app/src/main/java/com/movies/db/App.kt
@@ -3,7 +3,7 @@ package com.movies.db
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
-
+    
 @HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
