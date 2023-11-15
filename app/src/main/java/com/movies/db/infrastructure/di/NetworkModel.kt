3cd9f4f110973b9a7f0ed2e05b93b78d816f4e7a
Samuel Paez <samuelpaez@bdp.com.do>
2023-11-15 13:31:32 -0400

    data source

--- app/src/main/java/com/movies/db/infrastructure/di/NetworkModel.kt
+++ app/src/main/java/com/movies/db/infrastructure/di/NetworkModel.kt
@@ -7,9 +7,6 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
