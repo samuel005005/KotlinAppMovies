a7178d4b64bd7694bdca2cf8b3215e7a01c6283e
Samuel Paez <samuelpaez@bdp.com.do>
2023-11-16 10:33:32 -0400

    adding horizontal carrusel

--- app/src/main/java/com/movies/db/movies/presentation/views/CarouselCard.kt
+++ app/src/main/java/com/movies/db/movies/presentation/views/CarouselCard.kt
@@ -2,20 +2,34 @@ package com.movies.db.movies.presentation.views
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import coil.size.Scale
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.calculateCurrentOffsetForPage
 import com.google.accompanist.pager.rememberPagerState
+import com.movies.db.R
+import kotlin.math.absoluteValue
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
@@ -31,19 +45,54 @@ fun CarouselCard() {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        HorizontalPager(
-            count = sliderList.size,
-            state = pagerState,
-            contentPadding = PaddingValues(horizontal = 40.dp),
-            modifier = Modifier.height(250.dp)
-        ) { page ->
-            Card(
-                shape = RoundedCornerShape(10.dp)
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(
+                onClick = { /*TODO*/ }
             ) {
-                AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current).data(sliderList[page])
-                        .crossfade(true).scale(Scale.FILL).build(), contentDescription = null
-                )
+                Icon(Icons.Default.KeyboardArrowLeft, null)
+            }
+            HorizontalPager(
+                count = sliderList.size,
+                state = pagerState,
+                contentPadding = PaddingValues(horizontal = 65.dp),
+                modifier = Modifier.height(300.dp)
+            ) { page ->
+                Card(
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier.graphicsLayer {
+                        val pageOffset = calculateCurrentOffsetForPage(page = page).absoluteValue
+                        lerp(
+                            start = 0.85f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        ).also { scale ->
+                            scaleX = scale
+                            scaleY = scale
+                        }
+                        alpha = lerp(
+                            start = 0.5f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        )
+                    }
+                ) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current).data(sliderList[page])
+                            .crossfade(true).scale(Scale.FILL).build(),
+                        contentDescription = null,
+                        placeholder = painterResource(id = R.drawable.placeholder),
+                        error = painterResource(id = R.drawable.error)
+                    )
+                }
+            }
+            IconButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(Icons.Default.KeyboardArrowRight, null)
             }
         }
     }
