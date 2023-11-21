7388018720d260a0828a6e63995933a8460497de
Samuel Paez <samuelpaez@bdp.com.do>
2023-11-21 13:34:14 -0400

    realm implemented

--- app/src/main/java/com/movies/db/shared/infrastructure/core/modules/MoviesBDModule.kt
+++ app/src/main/java/com/movies/db/shared/infrastructure/core/modules/MoviesBDModule.kt
@@ -2,5 +2,5 @@ package com.movies.db.shared.infrastructure.core.modules
 
 import io.realm.annotations.RealmModule
 
-@RealmModule(library = true)
+@RealmModule(library = true, allClasses = true)
 class MoviesBDModule
--- app/src/main/java/com/movies/db/shared/infrastructure/core/modules/MoviesCacheModule.kt
+++ app/src/main/java/com/movies/db/shared/infrastructure/core/modules/MoviesCacheModule.kt
@@ -3,5 +3,5 @@ package com.movies.db.shared.infrastructure.core.modules
 import com.movies.db.movies.infrastructure.data.cache.MovieEntity
 import io.realm.annotations.RealmModule
 
-@RealmModule(library = true)
+@RealmModule(library = true, classes = [MovieEntity::class])
 class MoviesCacheModule
