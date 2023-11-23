14941051896c4f1dd747ca0a47c8b807c785542b
Samuel Paez <spaez@viva.com.do>
2023-11-22 23:25:37 -0400

    Paginated Movies Categories

--- app/src/main/java/com/movies/db/movies/presentation/MoviesViewModel.kt
+++ app/src/main/java/com/movies/db/movies/presentation/MoviesViewModel.kt
@@ -9,6 +9,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.launch
 import androidx.compose.runtime.State
+import com.movies.db.movies.domain.entities.Movie
 import com.movies.db.shared.domain.core.Resource
 import com.movies.db.shared.presentation.util.DefaultPaginator
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -25,37 +26,44 @@ class MoviesViewModel @Inject constructor(private val getNowPlayingUseCase: GetN
     val nowPlayingMovies: StateFlow<MoviesState> = _nowPlayingMovies.asStateFlow()
 
 
-    val paginator = DefaultPaginator(
-        initialKey = _nowPlayingMovies.value.page,
-        onLoadUpdated = {
-            _nowPlayingMovies.value = _nowPlayingMovies.value.copy(isLoading = it)
-        },
-        onRequest = { nextPage ->
-            getNowPlayingUseCase(nextPage, true)
-        },
-        getNextKey = {
-            _nowPlayingMovies.value.page + 1
-        },
-        onError = {
-            _nowPlayingMovies.value = _nowPlayingMovies.value.copy(error = it?.localizedMessage)
-        },
-        onSuccess = { movies, newPage ->
-            _nowPlayingMovies.value = _nowPlayingMovies.value.copy(
-                movies = _nowPlayingMovies.value.movies + movies,
-                page = newPage,
-                endReached = movies.isNotEmpty(),
-            )
-        }
-    )
-
     init {
         getNowPlaying()
     }
 
     fun getNowPlaying() {
         viewModelScope.launch {
-            paginator.loadNextItems()
+            paginator(states = _nowPlayingMovies, useCase =
+            { nextPage, refresh ->
+                getNowPlayingUseCase(nextPage, refresh)
+            }).loadNextItems()
         }
     }
 
+    private fun paginator(
+        states: MutableStateFlow<MoviesState>,
+        useCase: suspend (nextKey: Int, refresh: Boolean) -> Resource<List<Movie>>,
+    ): DefaultPaginator<Int, Movie> {
+        return DefaultPaginator(
+            initialKey = states.value.page,
+            onLoadUpdated = {
+                states.value = states.value.copy(isLoading = it)
+            },
+            onRequest = { nextPage ->
+                useCase(nextPage, true)
+            },
+            getNextKey = {
+                states.value.page + 1
+            },
+            onError = {
+                states.value = states.value.copy(error = it?.localizedMessage)
+            },
+            onSuccess = { movies, newPage ->
+                states.value = states.value.copy(
+                    movies = states.value.movies + movies,
+                    page = newPage,
+                    endReached = movies.isNotEmpty(),
+                )
+            }
+        )
+    }
 }
